516d2786f19590fd37d8eefbb4ed5d5253eb7188
Jlassi Mohamed Aziz <azizjlassi498@gmail.com>
2024-10-08 16:45:33 +0100

    Ajout du méthode POST

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express")
-const products = require("./products.json")
+const fs = require("fs") 
 
 
 
 const app = express()
+app.use(express.json())
 
 const port = 3000
 
@@ -13,11 +14,39 @@ app.listen(port,()=>{
     console.log("Server is running on port : " + port)
 })
 
-app.get("/products",(req,res)=>{
+const products = JSON.parse(fs.readFileSync("./products.json","utf-8"));
 
-    
-    
+app.get("/products",(req,res)=>{
+    res.status(200).json({
+        status:"success",
+        results:products.length,
+        data:products
+    })
     res.status(200).json(products);
 
 })
+
+app.post("/products",(req,res)=>{
+    // création d'un id incrémenté automatiquement 
+    const id = products[products.length - 1 ].id + 1;
+    
+    const newProduct  = Object.assign({"id":id} , req.body);
+
+    products.push(newProduct);
+    fs.writeFile("./products.json",JSON.stringify(products),"utf-8",(err)=>{
+        res.status(201).json({
+            status: "success",
+            message: "Product created !",
+            data:[newProduct]
+        })
+    })
+
+
+
+
+   
+})
+    
+    
+
 
